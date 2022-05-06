d5c5de253a15e340c7721aabd27ecfd279855513
alvaro <daw202328@estudiantes.salesianasnsp.es>
2022-05-06 09:02:47 +0200

    clases vacias

---
+++ LDF/src/mdulos/Pelicula.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mdulos;
+
+/**
+ *
+ * @author administrador
+ */
+public class Pelicula {
+    private String nombre;
+    private String director;
+    private String generos;
+    private int duracion;
+    private String 
+    private LocalDate fecha_estreno;
+}
